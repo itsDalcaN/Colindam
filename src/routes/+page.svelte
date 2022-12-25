3fbfd38c2c05b79245774cdce9aa44ae0cce97a1
Dan Sumindan <dansumindan@gmail.com>
2022-12-25 15:28:01 -0500

    Test mainline restriction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,27 +12,3 @@
   consequuntur velit asperiores culpa officiis perferendis. Facere molestiae quisquam deserunt
   accusamus beatae.
 </p>
-<p>
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis totam aliquam, minima
-  corporis veritatis inventore suscipit voluptas recusandae amet doloremque minus fuga est a
-  pariatur. Obcaecati corporis deleniti debitis unde minus dignissimos neque odit ut possimus quos!
-  Corrupti consequuntur praesentium sequi ad nobis! Aut sint, officiis incidunt consequuntur quaerat
-  ullam odit, quis omnis harum, ipsum saepe? Dolor officia molestiae quidem dignissimos. Vel nisi
-  reiciendis quia recusandae. Animi in asperiores dolorum mollitia quam esse velit eius, repellat
-  natus doloribus iure excepturi blanditiis iusto eligendi! Facilis laboriosam repellat similique
-  quo reprehenderit. Tempora incidunt similique ipsum molestiae porro natus numquam minus odit
-  architecto.
-</p>
-<ul>
-  <li>test 1</li>
-  <li>test 1</li>
-  <li>test 1</li>
-</ul>
-<blockquote cite="http://www.worldwildlife.org/who/index.html">
-  For 60 years, WWF has worked to help people and nature thrive. As the world's leading conservation
-  organization, WWF works in nearly 100 countries. At every level, we collaborate with people around
-  the world to develop and deliver innovative solutions that protect communities, wildlife, and the
-  places in which they live.
-</blockquote>
-
-<p>WWF's goal is to: <q>Build a future where people live in harmony with nature.</q></p>
