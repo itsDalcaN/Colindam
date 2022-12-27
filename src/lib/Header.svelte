fb5ee4a974aec152a4f45dd388c000e2f8e4498f
Dan Sumindan <dansumindan@gmail.com>
2022-12-27 12:37:02 -0500

    Remove horizontal scrollbar

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,9 +4,15 @@
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1 font-semibold text-primary-content">
-      <li><a href="/houses">Houses</a></li>
+      <!-- <li><a href="/houses">Houses</a></li> -->
       <li><a href="/lyrics">Lyrics</a></li>
       <li><a href="/settings"><span class="material-symbols-outlined"> settings </span></a></li>
     </ul>
   </div>
 </div>
+
+<style lang="scss">
+  .material-symbols-outlined {
+    font-variation-settings: 'FILL' 1, 'wght' 400, 'GRAD' 200, 'opsz' 48;
+  }
+</style>
