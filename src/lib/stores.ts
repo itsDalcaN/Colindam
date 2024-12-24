1741f3e3543f8996c04c31b27d66ce3e8579a4e4
Dan Sumindan <dansumindan@gmail.com>
2024-12-23 19:58:08 -0500

    Implement fuzzy search

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const autoExpandOnSearchSetting = writable(true);
+export const requireExactMatchSetting = writable(false);
 export const sortSongsAlphabeticallySetting = writable(true);
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-  import { autoExpandOnSearchSetting, sortSongsAlphabeticallySetting } from '$lib/stores';
+  import {
+    autoExpandOnSearchSetting,
+    sortSongsAlphabeticallySetting,
+    requireExactMatchSetting,
+  } from '$lib/stores';
   import SettingRow from '../../lib/SettingRow.svelte';
 </script>
 
 <h2 class="text-center">Settings</h2>
 <div class="flex flex-col m-auto xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-2/3">
   <SettingRow title="Auto Expand Lyrics On Search" settingStore={autoExpandOnSearchSetting} />
-  <SettingRow title="Sort Songs Alphabetically" settingStore={sortSongsAlphabeticallySetting} />
+  <SettingRow title="Require Exact Match On Search" settingStore={requireExactMatchSetting} />
+  <!-- <SettingRow title="Sort Songs Alphabetically" settingStore={sortSongsAlphabeticallySetting} /> -->
 </div>
